5e5b1a2ffc7722bf4c45f25e0c457a5cfd5b6971
Adam Lindqvist <adamlindqvist@outlook.com>
2017-08-11 09:56:08 +0200

    Update CookieHelper.ts

--- CookieHelper.ts
+++ CookieHelper.ts
@@ -19,7 +19,7 @@ export default class CookieHelper {
     }
 
     /**
-     * Read cookie with a specific name.
+     * Get cookie value.
      * @param name Name of the cookie to read.
      */
     static read(name: string) {
@@ -42,7 +42,7 @@ export default class CookieHelper {
     }
 
     /**
-     * Erase specific cookie.
+     * Remove a cookie.
      * @param name Name of the cookie to remove.
      */
     static erase(name: string) {
